c36ceae1b866317f9e6ce42eff64c989c7e5bfba
binhsonnguyen <son@binhsonnguyen.com>
2018-01-22 21:16:47 +0700

    self introduction on `about` page ---
    Ở revision này, web app phản hồi thêm thông tin giới thiệu nếu client
    truy cập vào đường dẫn `/about`. Ví dụ `http://localhost:8000/about`.

--- lesson-01-http-server/greeting.js
+++ lesson-01-http-server/greeting.js
@@ -4,8 +4,12 @@ let http = require('http')
 
 let app = http.createServer(function (req, res) {
   console.log('got a request for ' + req.url + ' from ' + req.headers['user-agent'])
+  let content = '<h2>hello world!</h2>'
+  if (req.url.toLowerCase() === '/about') {
+    content += '<br/><p>My name is Siri</p>'
+  }
   res.writeHead(200, {'Content-Type': 'text/html'})
-  res.end('<h2>hello world!</h2>')
+  res.end(content)
 })
 
 const PORT = 8000
