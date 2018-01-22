d78b7887e1bddabcf941037f45e064474970145f
binhsonnguyen <son@binhsonnguyen.com>
2018-01-22 21:05:59 +0700

    reponse a html content to client ---
    Ở revision này, web app gửi tới client một response với nội dung là một
    phần mã HTML.

--- lesson-01-http-server/greeting.js
+++ lesson-01-http-server/greeting.js
@@ -5,7 +5,7 @@ let http = require('http')
 let app = http.createServer(function (req, res) {
   console.log('got a request for ' + req.url + ' from ' + req.headers['user-agent'])
   res.writeHead(200, {'Content-Type': 'text/plain'})
-  res.end('hello world!')
+  res.end('<h2>hello world!</h2>')
 })
 
 const PORT = 8000
